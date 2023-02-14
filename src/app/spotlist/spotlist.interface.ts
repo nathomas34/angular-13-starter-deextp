6eeaf0d2cd0676944c119419eaa2f335f685c062
nathomas34 <61149320+nathomas34@users.noreply.github.com>
2023-02-14 14:25:47 +0100

    1 file modified, 1 added

--- src/app/spotlist/spotlist.interface.ts
+++ src/app/spotlist/spotlist.interface.ts
@@ -89,7 +89,7 @@ export interface TotalSpotNumber {
   metadata: Metadata9;
 }
 
-export interface Spot {
+export interface Spotlist {
   id: string;
   type: string;
   allowedVehicleType: AllowedVehicleType;
