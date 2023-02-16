bc6d21edf477778be35e27f1fa76561a5293c3a1
nathomas34 <61149320+nathomas34@users.noreply.github.com>
2023-02-16 09:32:50 +0100

    2 files modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { SpotlistComponent } from './spotlist/spotlist.component';
 import { Service } from './service.service';
+import { SpotitemComponent } from './spot-item/spotitem.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule, HttpClientModule],
-  declarations: [AppComponent, SpotlistComponent],
+  declarations: [AppComponent, SpotlistComponent, SpotitemComponent],
   providers: [Service],
   exports: [],
   bootstrap: [AppComponent],
---
+++ src/app/spot-item/spotitem.component.ts
@@ -0,0 +1,11 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Spotlist } from '../spotlist/spotlist.interface';
+
+@Component({
+  selector: 'spotitem',
+  templateUrl: './spotitem.component.html',
+  styleUrls: ['./spotitem.component.css'],
+})
+export class SpotitemComponent {
+  @Input() spot: Spotlist;
+}
